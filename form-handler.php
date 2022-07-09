7e1c12a0ffa87bdf63b84416bbf1042753f6f2e2
pantvijay <96246595+pantvijay@users.noreply.github.com>
2022-07-09 17:42:12 +0530

    my own website

---
+++ form-handler.php
@@ -0,0 +1,18 @@
+<?php
+$name=$_POST['name'];
+$visitor_email=$_POST['email'];
+$message=$_POST['message'];
+
+$email_from='worldofkavita.com';
+$email_subject='New Form Submission';
+$email_body="User Name:$name.\n".
+"User Name:$visitor_email.\n".
+"User Name:$message.\n";
+
+$to ='kavitapant1309@gmail.com';
+$headers= "From: $email_from \r\n";
+$headers= "Reply-To: $visitor_email \r\n";
+mail($to,$email_subject,$email_body,$headers);
+header("Location:contact.html");
+
+?>
